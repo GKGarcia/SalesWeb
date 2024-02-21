e9df6bcab33fa62bbc59f03031c024d96e6a9aa5
GKGarcia <gabrielkrutsch963@gmail.com>
2024-02-21 20:39:26 -0300

    Projected Creted

---
+++ SaleswebMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SaleswebMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
